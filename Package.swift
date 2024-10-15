c3207d44f61bbffd821ce659dbde12fa318418ba
Ryan Francesconi <ryan@add.app>
2024-10-15 08:20:22 -0700

    swift atomics 1.1+

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.1.0")
     ],
     
     targets: [
